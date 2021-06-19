3135d524b0c1a0374f6a9ad86cfdfb8d67df4814
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-19 09:15:06 +0530

    Deleted everything

--- src/cse_lk/_constants.py
+++
@@ -1,8 +0,0 @@
-"""Constants."""
-import os
-
-URL_DAILY_SUMMARY = os.path.join(
-    'https://www.cse.lk',
-    'pages/trade-summary/trade-summary.component.html',
-)
-CACHE_NAME, CACHE_TIMEOUT = 'cse_lk', 3600
--- src/cse_lk/upload_data.py
+++
@@ -1,6 +0,0 @@
-"""Upload data."""
-
-from cse_lk import daily_summary
-
-if __name__ == '__main__':
-    daily_summary.dump()
