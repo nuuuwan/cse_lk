58b8cdc526ae438436a7ddf7d79907d522d602fe
nuuuwan <nuuuwan@gmail.com>
2023-01-21 14:49:18 +0530

    perf: added cached_property

--- src/cse_lk/core/StockStatistics.py
+++ src/cse_lk/core/StockStatistics.py
@@ -1,35 +1,36 @@
 import math
 import statistics
+from functools import cached_property
 
 
 class StockStatistics:
-    @property
+    @cached_property
     def price_list(self):
         return [d.price_last_traded for d in self.daily_summary_list]
 
-    @property
+    @cached_property
     def mean(self):
         return statistics.mean(self.price_list)
 
-    @property
+    @cached_property
     def stdev(self):
         return statistics.stdev(self.price_list)
 
-    @property
+    @cached_property
     def p_delta_price_list(self):
         return sorted([d.p_delta_price for d in self.daily_summary_list])
 
-    @property
+    @cached_property
     def latest_p_delta_price(self):
         return self.latest_daily_summary.p_delta_price
 
-    @property
+    @cached_property
     def latest_p_p_delta_price(self):
         return self.latest_daily_summary.get_p_p_delta_price(
             self.p_delta_price_list
         )
 
-    @property
+    @cached_property
     def latest_p_p_delta_price_human(self):
         latest_p_p_delta_price = self.latest_p_p_delta_price
         if latest_p_p_delta_price > 0.5:
@@ -42,7 +43,7 @@ class StockStatistics:
         log_alpha = -math.log10(alpha)
         return (int)(log_alpha) * sign
 
-    @property
+    @cached_property
     def is_latest_significant(self):
         limit = 0.01
         return (
