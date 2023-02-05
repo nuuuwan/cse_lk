fb8f821029d3dca0eb5a31a5a7d26e35c3cc7c23
nuuuwan <nuuuwan@gmail.com>
2023-02-05 09:47:23 +0530

    feat: + CSEWebstieHome

---
+++ src/cse_lk/core/CSEWebsiteHome.py
@@ -0,0 +1,102 @@
+import os
+from utils import (
+    Browser,
+    Log,
+    String,
+    TimeFormat,
+    TSVFile,
+    TIME_FORMAT_DATE_ID,
+    Time,
+)
+from bs4 import BeautifulSoup
+from cse_lk.core.DailySummary import DailySummary
+
+log = Log('CSEWebsiteDailySummary')
+
+
+class CSEWebsiteDailySummary:
+    URL = os.path.join(
+        'https://www.cse.lk',
+        'pages',
+        'trade-summary',
+        'trade-summary.component.html',
+    )
+
+    @property
+    def html(self):
+        browser = Browser()
+        browser.open(self.URL)
+        browser.find_element(
+            "xpath",
+            "//select[@name='DataTables_Table_0_length']/option[text()='All']",
+        ).click()
+        html = browser.source
+        browser.quit()
+        return html
+
+    @property
+    def daily_summary_list(self):
+        soup = BeautifulSoup(self.html, 'html.parser')
+
+        span_updated_time = soup.find('span', class_='updated-time')
+        ut = (
+            TimeFormat('MARKET STATISTICS AS OF %b %d, %Y, %I:%M:%S %p')
+            .parse(span_updated_time.text.strip())
+            .ut
+        )
+
+        daily_summary_list = []
+        for i_row, tr_company_row in enumerate(soup.find_all('tr')):
+            if i_row == 0:
+                continue
+            
+            [
+                _,  # name,
+                symbol,
+                share_volume,
+                trade_volume,
+                price_previous_close,
+                price_open,
+                price_high,
+                price_low,
+                price_last_traded,
+                _,  # delta_price,
+                _,  # delta_price_p,
+            ] = list(
+                map(
+                    lambda td_cell: td_cell.text.strip(),
+                    tr_company_row,
+                )
+            )
+
+            daily_summary_list.append(
+                DailySummary(
+                    ut=ut,
+                    symbol=symbol,
+                    share_volume=String(share_volume).int,
+                    trade_volume=String(trade_volume).int,
+                    price_previous_close=String(price_previous_close).float,
+                    price_open=String(price_open).float,
+                    price_high=String(price_high).float,
+                    price_low=String(price_low).float,
+                    price_last_traded=String(price_last_traded).float,
+                )
+            )
+
+        return daily_summary_list
+
+    def parse_and_save(self):
+        daily_summary_list = self.daily_summary_list
+        ut = daily_summary_list[0].ut
+        date_id = TIME_FORMAT_DATE_ID.stringify(Time(ut))
+        daily_summary_file_path = f'/tmp/cse_lk.daily_summary.{date_id}.tsv'
+
+        d_list = [d.to_dict() for d in daily_summary_list]
+
+        TSVFile(daily_summary_file_path).write(d_list)
+        n = len(daily_summary_list)
+        log.info(f'Wrote {n} rows to {daily_summary_file_path}')
+
+
+if __name__ == '__main__':
+    print(CSEWebsiteDailySummary().parse_and_save())
