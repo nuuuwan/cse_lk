bf10263c0af2dabf4ed2bbcf9bdb3b71edd3a314
nuuuwan <nuuuwan@gmail.com>
2023-02-05 09:56:13 +0530

    various

--- src/cse_lk/core/DailySummary.py
+++ src/cse_lk/core/DailySummary.py
@@ -2,27 +2,6 @@ from dataclasses import dataclass
 
 from utils import String, Time, TimeFormat
 
-
-def get_i_equal_list(value, value_list):
-    return [x[0] for x in enumerate(value_list) if x[1] == value]
-
-
-def get_i_more(value, value_list):
-    more_list = [x[0] for x in enumerate(value_list) if x[1] > value]
-    if not more_list:
-        return None
-    return more_list[0]
-
-
-def get_p_mid(index_list, n):
-    n_list = len(index_list)
-    if n_list % 2 == 1:
-        i_mid = index_list[(n_list - 1) // 2]
-    else:
-        i_mid = index_list[n_list // 2] - 0.5
-    return i_mid / n
-
-
 @dataclass
 class DailySummary:
     ut: int
@@ -44,21 +23,6 @@ class DailySummary:
     def delta_price(self):
         return self.price_last_traded - self.price_previous_close
 
-    @property
-    def p_delta_price(self):
-        return self.delta_price / max(1, self.price_previous_close)
-
-    def get_p_p_delta_price(self, p_delta_price_list):
-        n = len(p_delta_price_list)
-        p_delta_price = self.p_delta_price
-        i_equal_list = get_i_equal_list(p_delta_price, p_delta_price_list)
-        if i_equal_list:
-            return get_p_mid(i_equal_list, n)
-        i_more = get_i_more(p_delta_price, p_delta_price_list)
-        if i_more:
-            return i_more / n
-
-        return 1.0
 
     @staticmethod
     def from_dict(ut: int, d: dict):
