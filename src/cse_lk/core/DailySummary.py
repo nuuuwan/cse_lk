b6c6f922ba6b53c6db2056febe5baaf20bdcc477
nuuuwan <nuuuwan@gmail.com>
2023-02-05 10:27:51 +0530

    refac: Split DailySummary into DailySummaryBase

--- src/cse_lk/core/DailySummary.py
+++ src/cse_lk/core/DailySummary.py
@@ -1,54 +1,5 @@
-from dataclasses import dataclass
-
-from utils import String, Time, TimeFormat
+from cse_lk.core.DailySummaryBase import DailySummaryBase
 
 
-@dataclass
-class DailySummary:
-    ut: int
-    symbol: str
-    name: str
-    share_volume: int
-    trade_volume: int
-    price_previous_close: float
-    price_open: float
-    price_high: float
-    price_low: float
-    price_last_traded: float
-
-    @property
-    def date(self):
-        return TimeFormat('%Y-%m-%d').stringify(Time(self.ut))
-
-    @property
-    def delta_price(self):
-        return self.price_last_traded - self.price_previous_close
-
-    @staticmethod
-    def from_dict(d: dict):
-        return DailySummary(
-            ut=String(d['ut']).int,
-            symbol=d['symbol'],
-            name=d['name'],
-            share_volume=String(d['share_volume']).int,
-            trade_volume=String(d['trade_volume']).int,
-            price_previous_close=String(d['price_previous_close']).float,
-            price_open=String(d['price_open']).float,
-            price_high=String(d['price_high']).float,
-            price_low=String(d['price_low']).float,
-            price_last_traded=String(d['price_last_traded']).float,
-        )
-
-    def to_dict(self):
-        return {
-            'ut': self.ut,
-            'name': self.name,
-            'symbol': self.symbol,
-            'share_volume': self.share_volume,
-            'trade_volume': self.trade_volume,
-            'price_previous_close': self.price_previous_close,
-            'price_open': self.price_open,
-            'price_high': self.price_high,
-            'price_low': self.price_low,
-            'price_last_traded': self.price_last_traded,
-        }
+class DailySummary(DailySummaryBase):
+    pass
