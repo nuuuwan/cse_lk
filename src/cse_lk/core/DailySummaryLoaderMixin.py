69c4e53690a310328298bba1f7badc7c876ea479
nuuuwan <nuuuwan@gmail.com>
2023-02-05 10:57:16 +0530

    feat: list methods

--- src/cse_lk/core/DailySummaryLoaderMixin.py
+++ src/cse_lk/core/DailySummaryLoaderMixin.py
@@ -1,11 +1,16 @@
 import os
 
-from utils import WWW
+from utils import WWW, Directory, Git, Log, TimeFormat, TSVFile
 
 from cse_lk.core.CommonMixin import CommonMixin
 
+log = Log('DailySummaryLoaderMixin')
+
 
 class DailySummaryLoaderMixin(CommonMixin):
+    GIT_REPO_URL = 'https://github.com/nuuuwan/cse_lk'
+    DIR_URL = '/tmp/cse_lk'
+
     @staticmethod
     def get_url_for_date_id(date_id):
         return os.path.join(
@@ -22,3 +27,27 @@ class DailySummaryLoaderMixin(CommonMixin):
             DailySummaryLoaderMixin.get_url_for_date_id(date_id)
         ).readTSV()
         return [cls.from_dict(d) for d in d_list]
+
+    @classmethod
+    def list_all(cls):
+        git = Git(DailySummaryLoaderMixin.GIT_REPO_URL)
+        git.clone(DailySummaryLoaderMixin.DIR_URL, force=False)
+        git.checkout('data')
+
+        d_list = []
+        for child in Directory(DailySummaryLoaderMixin.DIR_URL).children:
+            if child.name.startswith(
+                'cse_lk.daily_summary.'
+            ) and child.name.endswith('.tsv'):
+                log.debug(f'Reading file {child.name}')
+                d_list_raw = TSVFile(child.path).read()
+                # FIX: legacy time format missing ut
+                ut = TimeFormat('%Y%m%d').parse(child.name[21:29]).ut
+                d_list_raw = [d | {'ut': str(ut)} for d in d_list_raw]
+                d_list += d_list_raw
+
+        return [cls.from_dict(d) for d in d_list]
+
+    @classmethod
+    def list_from_symbol(cls, symbol):
+        return [ds for ds in cls.list_all() if ds.symbol == symbol]
--- tests/test_daily_summary_loader_mixin.py
+++ tests/test_daily_summary_loader_mixin.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from cse_lk import DailySummary
 
@@ -7,3 +7,13 @@ class TestDailySummaryLoaderMixin(TestCase):
     def test_list_from_date_id(self):
         daily_summary_list = DailySummary.list_from_date_id('20230203')
         self.assertEqual(len(daily_summary_list), 252)
+
+    @skip('len becomes out of date')
+    def test_list_all(self):
+        daily_summary_list = DailySummary.list_all()
+        self.assertEqual(len(daily_summary_list), 72_485)
+
+    @skip('len becomes out of date')
+    def test_list_from_symbol(self):
+        daily_summary_list = DailySummary.list_from_symbol('ABAN.N0000')
+        self.assertEqual(len(daily_summary_list), 234)
