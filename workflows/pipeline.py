e3b13f4a050f3dd922513b57e27734b6d36b504e
nuuuwan <nuuuwan@gmail.com>
2023-02-06 16:48:29 +0530

    disable tweeting

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,8 +1,8 @@
-from cse_lk import CSEWebsiteDailySummary, MonthlyMovers
+from cse_lk import CSEWebsiteDailySummary
 
 if __name__ == '__main__':
     CSEWebsiteDailySummary().parse_and_save()
-    MonthlyMovers('gainers', 4).send()
-    MonthlyMovers('losers', 4).send()
-    MonthlyMovers('gainers', 12).send()
-    MonthlyMovers('losers', 12).send()
+    # MonthlyMovers('gainers', 4).send()
+    # MonthlyMovers('losers', 4).send()
+    # MonthlyMovers('gainers', 12).send()
+    # MonthlyMovers('losers', 12).send()
