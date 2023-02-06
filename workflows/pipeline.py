78d53581bbf08655fa0de690a2c570283a35a7e2
nuuuwan <nuuuwan@gmail.com>
2023-02-06 19:01:44 +0530

    re-enabled tweeting in pipeline

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,8 +1,12 @@
-from cse_lk import CSEWebsiteDailySummary
+from cse_lk import CSEWebsiteDailySummary, MonthlyMovers
 
 if __name__ == '__main__':
     CSEWebsiteDailySummary().parse_and_save()
-    # MonthlyMovers('gainers', 4).send()
-    # MonthlyMovers('losers', 4).send()
-    # MonthlyMovers('gainers', 12).send()
-    # MonthlyMovers('losers', 12).send()
+
+    try:
+        MonthlyMovers('gainers', 4).send()
+        MonthlyMovers('losers', 4).send()
+        MonthlyMovers('gainers', 12).send()
+        MonthlyMovers('losers', 12).send()
+    except BaseException:
+        pass
